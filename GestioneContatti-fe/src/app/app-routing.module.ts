1f2aa2923ae5961111f530e4336a7ec2674d5e03
Luca Piccinno <luca.pic1998@gmail.com>
2023-05-21 21:02:19 +0200

    working

--- GestioneContatti-fe/src/app/app-routing.module.ts
+++ GestioneContatti-fe/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppConstants } from './core/entities/app.contants';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: AppConstants.SECTION_ROUTE_PATHS.DASHBOARD,
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ GestioneContatti-fe/src/app/core/entities/app.contants.ts
@@ -0,0 +1,10 @@
+
+
+export class AppConstants{
+
+
+    public static readonly SECTION_ROUTE_PATHS = {
+        DASHBOARD: 'dashboard',
+
+    }
+}
